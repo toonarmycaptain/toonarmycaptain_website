a724a4f801417585e4ef71218b2dc46f16ca1891
David Antonini <toonarmycaptain@hotmail.com>
2020-09-13 22:28:51 -0500

    Skeleton pages with sticky menu bar, tab icon.
    Nav bar is sticky, underlines current page, highlights on mouseover.

--- app.py
+++ app.py
@@ -1,9 +1,56 @@
-from flask import Flask
+"""app.py"""
+from flask import Flask, redirect, url_for, render_template
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+
+@app.route('/favicon.ico', methods=['GET'])
+def favicon():
+    """
+    Serve webpage icon.
+    """
+    return redirect(url_for('static',
+                            filename='favicon48x48.ico',
+                            mimetype='image/vnd.microsoft.icon'
+                            )
+                    )
+
+
+@app.route('/', methods=['GET'])
+def base_url():
+    """Redirect bare url to home page."""
+    return redirect(url_for('home'), code=303)
+
+
+@app.route('/home/', methods=['GET'])
+def home():
+    """Home page."""
+    return render_template('home.html')
+
+
+@app.route('/projects/', methods=['GET'])
+def projects():
+    """Projects page."""
+    return render_template('projects.html', methods=['GET'])
+
+
+@app.route('/blog/', methods=['GET'])
+def blog():
+    """Blog page."""
+    return render_template('blog.html')
+
+
+@app.route('/contact/', methods=['GET'])
+def contact():
+    """Contact page."""
+    return render_template('contact.html')
+
+
+@app.route('/about/', methods=['GET'])
+def about():
+    """About page."""
+    return render_template('about.html')
+
 
 if __name__ == '__main__':
     app.run()
