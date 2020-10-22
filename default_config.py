d644e873c2d9daa919422beea4c7143b79c4c8ea
David Antonini <toonarmycaptain@hotmail.com>
2020-10-22 00:24:29 -0500

    Rename server addr, add pw/contact addr config.
    Rename CONTACT_EMAIL to SERVER_EMAIL_ADDRESS as it is the address the
    app will send from.
    Add SERVER_EMAIL_PASSWORD so that app can login to email address.
    Add CONTACT_EMAIL_ADDRESS configuring address contact messages will be
    sent to.
    Reorder to split general app config from email and sms specific config.

--- default_config.py
+++ default_config.py
@@ -1,5 +1,11 @@
 """Default application settings, to be overridden at runtime."""
 SECRET_KEY: bytes = b'some secret key'
-CONTACT_CELL_NUMBER: str = 'some number'
-CONTACT_EMAIL: str = 'some email'
+
 CONTACT_MESSAGE_MAX_LENGTH: int = 10000  # characters
+
+SERVER_EMAIL_ADDRESS: str = 'some email to send contact emails from'
+SERVER_EMAIL_PASSWORD = 'some good password'
+CONTACT_EMAIL_ADDRESS = 'where to sent contact emails to'
+
+
+CONTACT_CELL_NUMBER: str = 'some number'
