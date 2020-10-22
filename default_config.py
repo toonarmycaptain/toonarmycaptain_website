8995f75d934a54c2eb627a7136994ddf81d1ddbf
David Antonini <toonarmycaptain@hotmail.com>
2020-10-22 00:32:13 -0500

    Add missing typehints to default_config.py.

--- default_config.py
+++ default_config.py
@@ -4,8 +4,8 @@ SECRET_KEY: bytes = b'some secret key'
 CONTACT_MESSAGE_MAX_LENGTH: int = 10000  # characters
 
 SERVER_EMAIL_ADDRESS: str = 'some email to send contact emails from'
-SERVER_EMAIL_PASSWORD = 'some good password'
-CONTACT_EMAIL_ADDRESS = 'where to sent contact emails to'
+SERVER_EMAIL_PASSWORD: str = 'some good password'
+CONTACT_EMAIL_ADDRESS: str = 'where to sent contact emails to'
 
 
 CONTACT_CELL_NUMBER: str = 'some number'
