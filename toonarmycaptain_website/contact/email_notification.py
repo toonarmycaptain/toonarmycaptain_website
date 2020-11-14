5b5bde6ecad787b0d492c67b485a3682f3116b6c
David Antonini <toonarmycaptain@hotmail.com>
2020-11-13 23:24:09 -0600

    Refactor email_notification.py
    Factor out email message composition into compose_notification email.
    
    Make constants eg port, server email address, address to send
    notification to uppercased module constants.

--- toonarmycaptain_website/contact/email_notification.py
+++ toonarmycaptain_website/contact/email_notification.py
@@ -7,13 +7,13 @@ from flask import current_app as app
 
 """NB Keep actual account data secret, do not commit to github."""
 
-port = 465  # For SSL.
-from_address = app.config['SERVER_EMAIL_ADDRESS']
-to_address = app.config['CONTACT_EMAIL_ADDRESS']
-password = app.config['SERVER_EMAIL_PASSWORD']
+SSL_PORT = 465  # For SSL.
+FROM_ADDRESS = app.config['SERVER_EMAIL_ADDRESS']
+TO_ADDRESS = app.config['CONTACT_EMAIL_ADDRESS']
+PASSWORD = app.config['SERVER_EMAIL_PASSWORD']
 
 
-def send_contact_email(message_id: int, contact_email: str, contact_name: str, message_body: str):
+def send_contact_email(message_id: int, contact_email: str, contact_name: str, message_body: str) -> None:
     """
     Forward contact via email, from server address to contact address.
 
@@ -28,10 +28,30 @@ def send_contact_email(message_id: int, contact_email: str, contact_name: str, m
     # Create a secure SSL context
     context = ssl.create_default_context()
 
+    email_msg = compose_notification_email(contact_email, contact_name, message_body)
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", SSL_PORT, context=context) as server:
+        server.login(FROM_ADDRESS, PASSWORD)
+        server.send_message(from_addr=FROM_ADDRESS,
+                            to_addrs=TO_ADDRESS,
+                            msg=email_msg)
+    app.DATABASE.email_sent(message_id)
+
+
+def compose_notification_email(contact_email: str, contact_name: str, message_body: str) -> EmailMessage:
+    """
+    Compose notification email.
+
+    :param contact_email: str
+    :param contact_name: str
+    :param message_body: str
+    :return: EmailMessage
+    """
     email_msg = EmailMessage()
     email_msg['Subject'] = f'Contact from {contact_name}'
-    email_msg['From'] = from_address
-    email_msg['To'] = to_address
+    email_msg['From'] = FROM_ADDRESS
+    email_msg['To'] = TO_ADDRESS
+
     message_body = (f'{message_body}\n'
                     f'\n'
                     f'from {contact_name}\n'  # Option here to add ' a.k.a. {alternate_names}'
@@ -39,9 +59,4 @@ def send_contact_email(message_id: int, contact_email: str, contact_name: str, m
                     )
     email_msg.set_content(message_body)
 
-    with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-        server.login(from_address, password)
-        server.send_message(from_addr=from_address,
-                            to_addrs=to_address,
-                            msg=email_msg)
-    app.DATABASE.email_sent(message_id)
+    return email_msg
